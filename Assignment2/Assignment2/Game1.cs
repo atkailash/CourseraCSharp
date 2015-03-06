5bf776c893a61c33045db83aaf92cb2130fe4595
Brian <kailash@calliope-u>
2015-03-05 23:33:06 -0700

    Finished

--- Assignment2/Assignment2/Game1.cs
+++ Assignment2/Assignment2/Game1.cs
@@ -23,10 +23,13 @@ namespace ProgrammingAssignment2
         SpriteBatch spriteBatch;
 
         // STUDENTS: declare variables for three sprites
-
+		Texture2D sprite00;
+		Texture2D sprite01;
+		Texture2D sprite02;
 
         // STUDENTS: declare variables for x and y speeds
-
+		int x_speed;
+		int y_speed;
 
         // used to handle generating random values
         Random rand = new Random();
@@ -40,7 +43,7 @@ namespace ProgrammingAssignment2
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
+            Content.RootDirectory = "Assets";
 
             graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
             graphics.PreferredBackBufferHeight = WINDOW_HEIGHT; ;
@@ -69,10 +72,12 @@ namespace ProgrammingAssignment2
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // STUDENTS: load the sprite images here
-
+			sprite00 = Content.Load<Texture2D> ("sprite00.png");
+			sprite01 = Content.Load<Texture2D> ("sprite01.png");
+			sprite02 = Content.Load<Texture2D> ("sprite02.png");
 
             // STUDENTS: set the currentSprite variable to one of your sprite variables
-
+			currentSprite = sprite00;
         }
 
         /// <summary>
@@ -92,9 +97,10 @@ namespace ProgrammingAssignment2
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+           /* Commented out for Monogame. Put back in for XNA
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-
+			*/
             elapsedTime += gameTime.ElapsedGameTime.Milliseconds;
             if (elapsedTime > CHANGE_DELAY_TIME)
             {
@@ -102,39 +108,43 @@ namespace ProgrammingAssignment2
 
                 // STUDENTS: uncomment the code below and make it generate a random number 
                 // between 0 and 2 inclusive using the rand field I provided
-                //int spriteNumber = ;
+                int spriteNumber = rand.Next(3);
 
                 // sets current sprite
                 // STUDENTS: uncomment the lines below and change sprite0, sprite1, and sprite2
                 //      to the three different names of your sprite variables
-                //if (spriteNumber == 0)
-                //{
-                //    currentSprite = sprite0;
-                //}
-                //else if (spriteNumber == 1)
-                //{
-                //    currentSprite = sprite1;
-                //}
-                //else if (spriteNumber == 2)
-                //{
-                //    currentSprite = sprite2;
-                //}
+                if (spriteNumber == 0)
+                {
+                    currentSprite = sprite00;
+                }
+                else if (spriteNumber == 1)
+                {
+                    currentSprite = sprite01;
+                }
+                else if (spriteNumber == 2)
+                {
+                    currentSprite = sprite02;
+                }
 
                 // STUDENTS: set the drawRectangle.Width and drawRectangle.Height to match the width and height of currentSprite
-
+				drawRectangle.Width = currentSprite.Width;
+				drawRectangle.Height = currentSprite.Height;
 
                 // STUDENTS: center the draw rectangle in the window. Note that the X and Y properties of the rectangle
                 // are for the upper left corner of the rectangle, not the center of the rectangle
-
+				drawRectangle.X = (graphics.PreferredBackBufferWidth / 2) - (currentSprite.Width / 2);
+				drawRectangle.Y = (graphics.PreferredBackBufferHeight / 2) - (currentSprite.Height / 2);
 
                 // STUDENTS: write code below to generate random numbers  between -4 and 4 inclusive for the x and y speed 
 				// using the rand field I provided
                 // CAUTION: Don't redeclare the x speed and y speed variables here!
-
+				x_speed = rand.Next (-4, 5);
+				y_speed = rand.Next (-4, 5);
             }
 
             // STUDENTS: move the drawRectangle by the x speed and the y speed
-
+			drawRectangle.X = drawRectangle.X + x_speed;
+			drawRectangle.Y = drawRectangle.Y + y_speed;
 
             base.Update(gameTime);
         }
@@ -148,7 +158,11 @@ namespace ProgrammingAssignment2
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // STUDENTS: draw current sprite here
+			spriteBatch.Begin ();
 
+			spriteBatch.Draw (currentSprite, drawRectangle, Color.White);
+
+			spriteBatch.End ();
 
             base.Draw(gameTime);
         }
--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 #endregion
 
-namespace Assignment2
+namespace ProgrammingAssignment2
 {
     static class Program
     {
